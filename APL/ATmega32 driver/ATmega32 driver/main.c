bca2519ef8cca7c823ff9b46617ba0519e073a09
Mansour <mohammmmmmmmed852963@gmail.com>
2024-10-28 11:24:30 +0300

    Make a Button holding project with leds

--- APL/ATmega32 driver/ATmega32 driver/main.c
+++ APL/ATmega32 driver/ATmega32 driver/main.c
@@ -14,20 +14,46 @@
 int main(void)
 {
     /* Replace with your application code */
-	LED_Init(portc, pin0);
+	LED_Init(portc, pin0); // Green
+	LED_Init(portc, pin1); // Yellow
+	LED_Init(portc, pin2); // Red
 	BUTTON_Init(porta, pin0);
-	BUTTON_Enable_Internal_PullUp(porta, pin0);
+	
+	unsigned char counter = 0;
     while (1) 
     {
-		if(!BUTTON_Read(porta, pin0))
+		counter = 0;
+		
+		LED_Off(portc, pin0);
+		LED_Off(portc, pin1);
+		LED_Off(portc, pin2);
+		
+		while(BUTTON_Read(porta, pin0))
 		{
-			_delay_ms(500);
-			LED_Off(portc, pin0);
-			_delay_ms(2000);
-		}
-		else
-		{
-			LED_On(portc, pin0);
+			_delay_ms(1000);
+			counter++;
+			
+			if(counter >= 5 && counter < 10)
+			{
+				LED_On(portc, pin0);
+				LED_Off(portc, pin1);
+				LED_Off(portc, pin2);
+				//_delay_ms(3000);
+			}
+			else if(counter >= 10 && counter < 15)
+			{
+				LED_Off(portc, pin0);
+				LED_On(portc, pin1);
+				LED_Off(portc, pin2);
+				//_delay_ms(3000);
+			}
+			else if(counter >= 15)
+			{
+				LED_Off(portc, pin0);
+				LED_Off(portc, pin1);
+				LED_On(portc, pin2);
+				//_delay_ms(3000);
+			}
 		}
     }
 }
