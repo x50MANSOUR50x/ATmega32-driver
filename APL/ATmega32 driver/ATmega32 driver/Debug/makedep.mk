bb5db4e847aaa366b31072e16afa8439d8ab0478
Mansour <mohammmmmmmmed852963@gmail.com>
2024-10-28 11:04:07 +0300

    Make a pull-down resistor button project

--- APL/ATmega32 driver/ATmega32 driver/Debug/makedep.mk
+++ APL/ATmega32 driver/ATmega32 driver/Debug/makedep.mk
@@ -2,6 +2,8 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
+BUTTON_program.c
+
 DIO_program.c
 
 LED_program.c
--- APL/ATmega32 driver/ATmega32 driver/main.c
+++ APL/ATmega32 driver/ATmega32 driver/main.c
@@ -9,32 +9,26 @@
 #include "util/delay.h"
 
 #include "LED_interface.h"
+#include "BUTTON_interface.h"
 
 int main(void)
 {
     /* Replace with your application code */
-	LED_Init(porta, pin0);
-	LED_Init(portb, pin0);
 	LED_Init(portc, pin0);
+	BUTTON_Init(porta, pin0);
+	
     while (1) 
     {
-		// Green led on
-		LED_On(porta, pin0);
-		LED_Off(portb, pin0);
-		LED_Off(portc, pin0);
-		_delay_ms(3000);
-		
-		//Yellow led on
-		LED_Off(porta, pin0);
-		LED_On(portb, pin0);
-		LED_Off(portc, pin0);
-		_delay_ms(1000);
-		
-		//Yellow led on
-		LED_Off(porta, pin0);
-		LED_Off(portb, pin0);
-		LED_On(portc, pin0);
-		_delay_ms(3000);
+		if(BUTTON_Read(porta, pin0))
+		{
+			_delay_ms(500);
+			LED_On(portc, pin0);
+			_delay_ms(2000);
+		}
+		else
+		{
+			LED_Off(portc, pin0);
+		}
     }
 }
 
