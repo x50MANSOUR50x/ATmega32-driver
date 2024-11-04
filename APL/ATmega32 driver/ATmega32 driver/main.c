9ed4a6c72782d340ad6050ddc34bb114379a2db8
Mansour <mohammmmmmmmed852963@gmail.com>
2024-11-04 14:06:46 +0200

    Big Project on all of the EXTIs

--- APL/ATmega32 driver/ATmega32 driver/main.c
+++ APL/ATmega32 driver/ATmega32 driver/main.c
@@ -11,30 +11,109 @@
 #include "avr/interrupt.h"
 
 #include "LED_interface.h"
+#include "7SEGMENT_interface.h"
 #include "EXTI_interface.h"
 
 
 int main(void)
 {
-	LED_Init(portc, pin1);
-	//LED_Init(porta, pin7);
+	unsigned char value;
+	
+	LED_Init(porta, pin1); // Green
+	LED_Init(porta, pin4); // Yellow
+	LED_Init(porta, pin7); // Red
+	
+	BCD_to_SEVEN_SEG_Init(portc, 0);
+	
 	EXTI_vEnable(EXTI0);
-	// EXTI_vActiveMode(EXTI0, LOW);
-	EXTI_vActiveMode(EXTI0, CHANGE);
-	// EXTI_vActiveMode(EXTI0, FALLING);
-	// EXTI_vActiveMode(EXTI0, RISING);
+	EXTI_vActiveMode(EXTI0, FALLING);	
+	EXTI_vEnable(EXTI1);
+	EXTI_vActiveMode(EXTI1, FALLING);	
+	EXTI_vEnable(EXTI2);
+	EXTI_vActiveMode(EXTI2, FALLING);
     while (1) 
     {
-		LED_On(portc, pin1);
+		for(value = 0; value < 10; value++)
+		{
+			BCD_to_SEVEN_SEG_Write(portc, 0, value);
+			_delay_ms(500);
+		}
 	}
 }
 
 ISR(INT0_vect)
 {
 	// EXTI_vDisable(EXTI0);
-	LED_Off(portc, pin1);
-	_delay_ms(3000);
+	for(unsigned char counter=0; counter < 4; counter++)
+	{
+		LED_On(porta, pin1);
+		LED_Off(porta, pin4);
+		LED_Off(porta, pin7);
+		_delay_ms(100);
+		LED_Off(porta, pin1);
+		_delay_ms(100);
+	}
+	for(unsigned char counter=0; counter < 4; counter++)
+	{
+		LED_Off(porta, pin1);
+		LED_On(porta, pin4);
+		LED_Off(porta, pin7);
+		_delay_ms(100);
+		LED_Off(porta, pin4);
+		_delay_ms(100);
+	}	
+	for(unsigned char counter=0; counter < 4; counter++)
+	{
+		LED_Off(porta, pin1);
+		LED_Off(porta, pin4);
+		LED_On(porta, pin7);
+		_delay_ms(100);
+		LED_Off(porta, pin7);
+		_delay_ms(100);
+	}
 	// EXTI_vEnable(EXTI0);
 }
+
+ISR(INT1_vect)
+{
+	// EXTI_vDisable(EXTI0);
+		LED_On(porta, pin1);
+		LED_Off(porta, pin4);
+		LED_Off(porta, pin7);
+		_delay_ms(2000);
+		LED_Off(porta, pin1);
+		LED_On(porta, pin4);
+		LED_Off(porta, pin7);
+		_delay_ms(1000);
+		LED_Off(porta, pin1);
+		LED_Off(porta, pin4);
+		LED_On(porta, pin7);
+		_delay_ms(2000);
+		LED_Off(porta, pin1);
+		LED_Off(porta, pin4);
+		LED_Off(porta, pin7);
+	// EXTI_vEnable(EXTI0);
+}
+
+ISR(INT2_vect)
+{
+	// EXTI_vDisable(EXTI0);
+	for(unsigned char counter=0; counter < 10; counter++)
+	{
+		LED_On(porta, pin1);
+		_delay_ms(150);
+		LED_Off(porta, pin1);
+		LED_On(porta, pin4);
+		_delay_ms(150);
+		LED_Off(porta, pin4);		
+		LED_On(porta, pin7);		
+		_delay_ms(150);
+		LED_Off(porta, pin7);
+	}
+	// EXTI_vEnable(EXTI0);
+}
+
+
+
 
 
